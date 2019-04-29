290d63b0804cf37d145b61dd77e73d5ba308298f
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 14:37:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,23 @@
 class Artist
-  attr_accessor :name, :genre, :songs
-  @@all=[]
-
+  attr_accessor :name
+ 
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
-  
+ 
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
-  def genres                        
+ 
+  def songs
+    @songs
+  end
+ 
+  def genres
     self.songs.collect do |song|
       song.genre
     end
   end
-
-  
-  def self.all
-    @@all
-  end
-
 end
