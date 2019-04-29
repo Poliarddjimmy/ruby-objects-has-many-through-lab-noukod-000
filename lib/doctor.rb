082159e8cc21c0edd9bbcf68de74400c3b53696b
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 14:53:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,13 +1,27 @@
 class Doctor
-  attr_accessor :name 
-  @@all = []
-  
-  def initialize(name)
-    @name=name
-    @@all << self
-  end
-  
+  @@all=[]
+
   def self.all
     @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
   end
 end
