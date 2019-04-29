7547518c3daca33918c04eb9b4ffee36dae99002
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 14:31:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,17 +7,11 @@ class Genre
     @name = name
     @songs = []
   end
+  
   def songs
     @songs
   end
 
-  def add_song(song)
-    @songs << song
-  end
-
-
   
-  def self.all
-    @@all
-  end
+
 end
