d2c529c4fcde65c0ac19c038724b619219a48313
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-29 14:36:45 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,21 +1,21 @@
 class Genre
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
     @songs = []
   end
-
+ 
   def songs
     @songs
   end
-
+ 
   def add_song(song)
     @songs << song
   end
-
-  def artists                    
-    self.songs.collect do |song|  
+ 
+  def artists
+    @songs.collect do |song|
       song.artist
     end
   end
